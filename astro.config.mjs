3529756dc544e44f8750daf9a6eb1c63248ad6dc
Arsani Guirguis <arsanig@users.noreply.github.com>
2026-02-25 01:20:43 -0500

    feat: Search on a separate page (#43)
    * fix: search on mobile
    
    * fix: more changes for mobile search
    
    * fix: more mobile search changes
    
    * fix: more changes for mobile search
    
    * fix: more mobile search changes
    
    * fix: more mobile search changes
    
    * fix: Search on a whole different page
    
    * feat: Search on a separate page
    
    * fix: spinner hidden on mobile
    
    * fix: Remove DOM injection, big uhoh
    
    * fix: faster loading for search
    
    * fix: synonymns
    
    * fix: way more fixes
    
    * fix: more synonyms and moved to separate file

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,9 @@ export default defineConfig({
         }),
         pagefind(),
     ],
+    image: {
+        domains: ["i.ytimg.com"],
+    },
     output: "static",
     build: {
         format: "file",
