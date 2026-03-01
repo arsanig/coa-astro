9a4b3efe4f3dca312affd4d6bc5de85dbe2bbdb9
arsani <arsanig@users.noreply.github.com>
2026-03-01 02:42:59 -0500

    fix: favicon, socials, logo, and animations

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import paraglide from "@inlang/paraglide-astro";
 import pagefind from "astro-pagefind";
 
+import favicons from "astro-favicons";
+
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
@@ -11,13 +13,15 @@ export default defineConfig({
         locales: ["en", "fr"],
         defaultLocale: "en",
     },
-    integrations: [
-        paraglide({
-            project: "./project.inlang",
-            outdir: "./src/paraglide",
-        }),
-        pagefind(),
-    ],
+    integrations: [paraglide({
+        project: "./project.inlang",
+        outdir: "./src/paraglide",
+    }), pagefind(), favicons({
+        name: "Coptic Orthodox Answers",
+        short_name: "COA",
+        background: "#171717",
+        theme_color: "#171717",
+    })],
     image: {
         domains: ["i.ytimg.com"],
     },
